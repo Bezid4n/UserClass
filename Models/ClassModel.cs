c5362ecc103a85627855ec6fcb93cbafd97bbaa8
software1 <software1@SOFTWARE1>
2021-04-07 14:28:28 +0430

    Add project files.

---
+++ Models/ClassModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserClass.Models
+{
+    public class ClassModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string Type { get; set; }
+
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserClass.Models
+{
+    public class UserModel
+    {
+        public string Name  { get; set; }
+        public string Family { get; set; }
+        public int Phone { get; set; }
+        public int Id { get; set; }
+        public string Address { get; set; }
+
+    }
+}
